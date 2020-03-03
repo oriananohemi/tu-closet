e6572f3ff52e6e0bcca87b2bced9d43e993160ec
Oriana Reina <oriana.nohemi22@gmail.com>
2020-03-02 21:25:44 -0500

    Agregar funcionalidad para el inicio de sesion

---
+++ js/sesion.js
@@ -0,0 +1,36 @@
+var logName = document.getElementById('loginName')
+var logPass = document.getElementById('loginPass')
+var loginUser = document.getElementById('loginUser')
+var logIn = document.getElementById('logIn')
+var logOut = document.getElementById('logOut')
+
+function loggedIn() {
+  if (localStorage.getItem('name')) {
+    loginUser.innerHTML = localStorage.getItem('name')
+    logOut.classList.remove('hidden')
+    logIn.classList.add('hidden')
+  } else {
+    alert('Debes iniciar sesion')
+  }
+}
+
+loggedIn()
+
+function logged() {
+  let passInput = document.getElementById('loginPass').value
+  if (passInput == 1234) {
+    loginUser.innerHTML = logName.value
+    localStorage.setItem('name', logName.value)
+    logIn.classList.add('hidden')
+    logOut.classList.remove('hidden')
+  } else {
+    alert('Contrasena Incorrecta')
+  }
+}
+
+function signOff() {
+  loginUser.innerHTML = ''
+  localStorage.clear()
+  logIn.classList.remove('hidden')
+  logOut.classList.add('hidden')
+}
