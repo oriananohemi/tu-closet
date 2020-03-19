19227acbbf74d80d09f2505f4ec2eab116fa4f48
Oriana Reina <oriana.nohemi22@gmail.com>
2020-03-18 22:28:34 -0500

    Agregar enlace desde perfil a la pagina principal

--- js/sesion.js
+++ js/sesion.js
@@ -1,31 +1,32 @@
-var logName = document.getElementById('loginName')
-var logPass = document.getElementById('loginPass')
-var loginUser = document.getElementById('loginUser')
-var logIn = document.getElementById('logIn')
-var logOut = document.getElementById('logOut')
+var logName = document.getElementById("loginName");
+var logPass = document.getElementById("loginPass");
+var loginUser = document.getElementById("loginUser");
+var logIn = document.getElementById("logIn");
+var logOut = document.getElementById("logOut");
 
 function loggedIn() {
-  if (localStorage.getItem('name')) {
-    loginUser.innerHTML = localStorage.getItem('name')
-    logIn.classList.add('hidden')
+  if (localStorage.getItem("name")) {
+    loginUser.innerHTML = localStorage.getItem("name");
+    logIn.classList.add("hidden");
   }
 }
 
-loggedIn()
+loggedIn();
 
 function logged() {
-  let passInput = document.getElementById('loginPass').value
+  let passInput = document.getElementById("loginPass").value;
   if (passInput == 1234) {
-    loginUser.innerHTML = logName.value
-    localStorage.setItem('name', logName.value)
-    logIn.classList.add('hidden')
+    loginUser.innerHTML = logName.value;
+    localStorage.setItem("name", logName.value);
+    logIn.classList.add("hidden");
   } else {
-    alert('Contrasena Incorrecta')
+    alert("Contrasena Incorrecta");
   }
 }
 
 function signOff() {
-  loginUser.innerHTML = ''
-  localStorage.clear()
-  logIn.classList.remove('hidden')
+  loginUser.innerHTML = "";
+  localStorage.clear();
+  logIn.classList.remove("hidden");
+  // window.location.href = "/";
 }
